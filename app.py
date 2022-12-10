aa2ad1b852d6f6aa68b0b05d885b9f7885c3b096
Faisal <faisal@sitech.me>
2022-12-10 14:13:56 +0300

    Fix typo

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Deloitte, welcome to my test case'
+    return 'Hello Deloitte, welcome to my test case\nPlease send raw string data to /replace and the response will contain the altered string'
 
 @app.route('/replace', methods=['POST'])
 def replace():
-    words=["Google", "Microsoft", "Amazon" ,"-+Deloitte", "Oracle"]
+    words=["Google", "Microsoft", "Amazon" ,"Deloitte", "Oracle"]
     rep=request.data
     rep=rep.decode("utf-8")
     for i in words:
